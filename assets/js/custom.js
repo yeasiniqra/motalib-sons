de664ae3b9d0a9eada35d1a91b740b8af1db1c9a
yeasiniqra <yeasin@iqrasys.com>
2024-05-05 20:13:27 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+	// $().click(function(){
+	// 		$().slideToggle(700);
+	// 		return false;
+	// });	
+
+	// $('.').click(function(){
+	// 	$("").slideToggle(700);
+	// 	return false;
+	// })
+
+		/*********** complete project slide ***************/
+		$('.project-main-grid').owlCarousel({
+			loop:true,
+			margin:true,
+			nav:true,
+			autoplay:true,
+			autoplayTimeout:4500,
+			dots:false,
+			items:3,
+				responsive:{
+				0:{
+					items:1
+				},
+				575:{
+					items:1
+				},
+				767:{
+					items:2
+				},
+				992:{
+					items:3
+				},
+				1200:{
+					items:3
+				}
+			}
+		})
+
+		/*********** complete project slide ***************/
+		$('.partner-main-grid').owlCarousel({
+			loop:true,
+			margin:false,
+			nav:false,
+			autoplay:true,
+			autoplayTimeout:4500,
+			dots:false,
+			items:5,
+				responsive:{
+				0:{
+					items:2
+				},
+				575:{
+					items:3
+				},
+				767:{
+					items:3
+				},
+				992:{
+					items:5
+				},
+				1200:{
+					items:5
+				}
+			}
+		})
+
+		/*********** complete project slide ***************/
+		$('.testmonial-main-grid').owlCarousel({
+			loop:true,
+			margin:false,
+			nav:false,
+			autoplay:true,
+			autoplayTimeout:5000,
+			dots:false,
+			items:3,
+				responsive:{
+				0:{
+					items:2
+				},
+				575:{
+					items:3
+				},
+				767:{
+					items:3
+				},
+				992:{
+					items:3
+				},
+				1200:{
+					items:3
+				}
+			}
+		})
+
+
+});
+
+
+
+
+
+
+		
+
+
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+"use strict";
+
+// menu sticky js
+var scrollpos = window.scrollY,
+    header = document.getElementById("header");
+
+function add_class_on_scroll() {
+    header.classList.add("sticky-menu");
+}
+
+function remove_class_on_scroll() {
+    header.classList.remove("sticky-menu");
+}
+
+window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos > 50) { 
+        add_class_on_scroll();
+    } else {
+        remove_class_on_scroll();
+    }
+});
+
+//counter up JS
+const c = (s, st, e, d) => {
+    const el = document.querySelector(s);
+    let t = null;
+
+    const a = (ts) => {
+        if (!t) t = ts;
+        const p = Math.min((ts - t) / d, 1);
+        el.innerText = Math.floor(p * (e - st) + st);
+        if (p < 1) window.requestAnimationFrame(a);
+    };
+
+    window.requestAnimationFrame(a);
+};
+
+const ac = () => {
+    c("#count1", 300, 10, 1000);
+    c("#count2", 5000, 4000, 1500);
+    c("#count3", 1000, 150, 2000);
+    c("#count4", 500, 10, 2500);
+    c("#count5", 500, 2, 2500);
+    c("#count6", 500, 96, 2500);
+};
+
+let is = false;
+
+const ca = () => {
+    if (is) return;
+    const se = document.querySelector("#scrollanime");
+    if (se.getBoundingClientRect().top + 50 <= (window.innerHeight || document.documentElement.clientHeight)) {
+        ac();
+        is = true;
+    }
+};
+
+window.addEventListener("scroll", ca);
+
+
+
